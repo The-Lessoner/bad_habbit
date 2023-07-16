1cee327d9ac4b8ca228e269c502545ee7712c298
GKurilchikova <Galina.Kurilchikova@gmail.com>
2023-07-16 17:01:01 +0300

    Added SignInAppleViewController

---
+++ BadHabbit/BadHabbit/UserStories/SingnInAppleScreen/SignInAppleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInAppleViewController.swift
+//  BadHabbit
+//
+//  Created by Halina Kurylchykava on 14.07.23.
+//
+
+import UIKit
+
+class SignInAppleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
