b55b0ca5da951b6fdadb44a9923f54e0e8d6fa89
GKurilchikova <Galina.Kurilchikova@gmail.com>
2023-07-19 19:38:45 +0300

    changed button constraints

--- BadHabbit/BadHabbit/UserStories/SingnInAppleScreen/SignInAppleViewController.swift
+++ BadHabbit/BadHabbit/UserStories/SingnInAppleScreen/SignInAppleViewController.swift
@@ -13,7 +13,7 @@ class SignInAppleViewController: UIViewController {
     private var authorizationButton: ASAuthorizationAppleIDButton!
     private var imageLogo: UIImageView!
     private var imageMountains: UIImageView!
-    private var titleLabel = UILabel()
+    private var titleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +26,10 @@ extension SignInAppleViewController {
     private func setupUI() {
         view.backgroundColor = UIColor(red: 0.81, green: 0.81, blue: 0.81, alpha: 1.00)
         imageMountains = createImageView(name: "mountains")
-        setupImageMountainsConstraints()
         imageLogo = createImageView(name: "logo")
-        setupImageLogoConstraints()
         createAuthorizationButton()
         createTitleLabel()
+        setupConstraints()
     }
 
     private func createImageView(name: String) -> UIImageView {
@@ -43,44 +42,46 @@ extension SignInAppleViewController {
     }
 
     private func createTitleLabel() {
-        titleLabel.frame = CGRect(x: 0, y: 0, width: 358, height: 36)
+        titleLabel = UILabel()
         view.addSubview(titleLabel)
         titleLabel.textColor = .black
         titleLabel.font = UIFont(name: "SFProDisplay-Bold", size: 24)
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.26
         let quote = "habit hero"
-        let attributes: [NSAttributedString.Key: Any]  = [ 
+        let attributes: [NSAttributedString.Key: Any]  = [
             .kern: -0.41,
             .paragraphStyle: paragraphStyle
         ]
         titleLabel.attributedText = NSMutableAttributedString(string: quote, attributes: attributes)
         titleLabel.text = titleLabel.text?.uppercased()
         titleLabel.textAlignment = .center
-        setupTitleLabelConstraints()
-
     }
 
-    private func setupTitleLabelConstraints() {
+    private func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+
         titleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(82)
             make.leading.trailing.equalToSuperview().inset(16)
         }
-    }
 
-    private func setupImageLogoConstraints() {
         imageLogo.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview().offset(-35)
             make.size.equalTo(196)
         }
-    }
 
-    private func setupImageMountainsConstraints() {
         imageMountains.snp.makeConstraints { make in
             let multiplier: CGFloat = 445/844
             make.height.equalToSuperview().multipliedBy(multiplier)
             make.bottom.leading.trailing.equalToSuperview()
+        }
+
+        authorizationButton.snp.makeConstraints { make in
+            make.bottom.equalTo(safeArea).inset(20)
+            make.height.equalTo(50)
+            make.leading.trailing.equalTo(safeArea).inset(16)
         }
     }
 
@@ -88,16 +89,7 @@ extension SignInAppleViewController {
         authorizationButton = ASAuthorizationAppleIDButton()
         view.addSubview(authorizationButton)
         authorizationButton.layer.cornerRadius = 12
-        setupAuthorizationButtonConstraints()
         authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
-    }
-
-    private func setupAuthorizationButtonConstraints() {
-        authorizationButton.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(54)
-            make.height.equalTo(50)
-            make.leading.trailing.equalToSuperview().inset(16)
-        }
     }
 
     @objc
