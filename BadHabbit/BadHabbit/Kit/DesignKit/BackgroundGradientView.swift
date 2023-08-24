f7891ac0e322aa8a5078af8d05a4ad18ff27e013
GKurilchikova <Galina.Kurilchikova@gmail.com>
2023-08-24 21:38:51 +0300

    fixed issues, added design

---
+++ BadHabbit/BadHabbit/Kit/DesignKit/BackgroundGradientView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundGradientView.swift
+//  BadHabbit
+//
+//  Created by Halina Kurylchykava on 24.08.23.
+//
+
+import UIKit
+
+class BackgroundGradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
